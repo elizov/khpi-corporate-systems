42fe58fde2977b1fe80ce59f126551e10b2d6dae
Sergey Elizov <elizovss@gmail.com>
2025-09-30 22:57:12 +0300

    products page

---
+++ app/src/main/java/com/shop/app/controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.shop.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+
+    @GetMapping("/products")
+    public String products() {
+        return "products";
+    }
+}
